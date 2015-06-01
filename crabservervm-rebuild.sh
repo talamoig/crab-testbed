53cc93424adba76b5e6c0bf85d5d165e07afbd35
Ivano Giuseppe Talamo <talamoig@lxplus0013.cern.ch>
2015-06-01 21:27:55 +0200

    bug fix

--- crabservervm-rebuild.sh
+++ crabservervm-rebuild.sh
@@ -3,8 +3,7 @@
 source crabservervm-common
 
 source ~/.openrc 2> /dev/null
-if [[ $? != 0 ]]; then echo "~/.openrc not present. create it with openstack-init.sh"; exit 1; fi
 
-IMAGE=`openstack image list -c Name -f csv|grep "SLC6 Server - x86_64"|head -1|tr -d '"'`
+if [[ $? != 0 ]]; then echo "~/.openrc not present. create it with openstack-init.sh"; exit 1; fi
 echo "Rebuilding with image $IMAGE"
 openstack server rebuild --image "$IMAGE" $VMNAME
