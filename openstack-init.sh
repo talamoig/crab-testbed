d528c0047e93c07d516457b7d4f46593001b0638
Ivano Giuseppe Talamo <talamoig@lxplus0029.cern.ch>
2015-06-16 15:09:30 +0200

    improved .openrc. if OS_PASSWOD

--- openstack-init.sh
+++ openstack-init.sh
@@ -2,14 +2,17 @@
 
 if [ ! -f ~/.openrc ]
 then
-cat > ~/.openrc <<EOF
+    cat > ~/.openrc <<EOF
 export OS_AUTH_URL=https://openstack.cern.ch:5000/v2.0
 export OS_USERNAME=`id -un`
-export OS_TENANT_NAME="Personal $OS_USERNAME"
-
-# With Keystone you pass the keystone password.
-echo "Please enter your OpenStack Password: "
-read -s OS_PASSWORD_INPUT
-export OS_PASSWORD=\$OS_PASSWORD_INPUT
+export OS_TENANT_NAME="Personal \$OS_USERNAME"
+if [ -z \$OS_PASSWORD ]
+then
+  echo "Please enter your OpenStack Password: "
+  read -s OS_PASSWORD_INPUT
+  export OS_PASSWORD=\$OS_PASSWORD_INPUT
+fi
 EOF
+else
+echo 'File ~/.openrc already present. Exiting...'
 fi
