51d0f4e955d561b960ce046c14f36f750b6ed440
Ivano Giuseppe Talamo <talamoig@lxplus0101.cern.ch>
2015-06-23 14:39:47 +0200

    made crabserver-install-part1.sh completely automated (if providing host key and cert)

--- crabserver-install-part1.sh
+++ crabserver-install-part1.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+CERT=certs/`hostname -s`-hostcert.pem
+KEY=certs/`hostname -s`-hostkey.pem
+mkdir /etc/grid-security
+if [ -f $CERT -a $KEY ]
+then
+sudo cp $CERT /etc/grid-security/hostcert.pem
+sudo cp $KEY /etc/grid-security/hostkey.pem
+sudo chmod 600 /etc/grid-security/hostkey.pem
+fi
 sudo yum -y install git.x86_64
 mkdir -p /tmp/foo
 cd /tmp/foo
 git clone git://github.com/dmwm/deployment.git cfg
-cfg/Deploy -t dummy -s post $PWD system/devvm
-#rm -fr /tmp/foo
-
-echo "You should now reboot, prepare paramsrc file and then launch part2"
+yes | cfg/Deploy -t dummy -s post $PWD system/devvm
+sudo reboot
